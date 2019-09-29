87d33b50efe1c0c4652c00e4e27c12e046b09e66
James Bradbury <jamesbradbury93@gmail.com>
2019-09-29 12:14:25 +0100

    can now get stat values

--- fluid-extra-centroid-sel.lua
+++ fluid-extra-centroid-sel.lua
@@ -5,6 +5,17 @@ dofile(script_path .. "FluidUtils.lua")
 
 --------------------------------------------------------------------------------------------------------------
 
+------------------------------------------------------------------------------------
+--   Each user MUST point this to their folder containing FluCoMa CLI executables --
+if sanity_check() == false then goto exit; end
+local cli_path = get_fluid_path()
+--   Then we form some calls to the tools that will live in that folder --
+local ss_suf = cli_path .. "/fluid-spectralshape"
+local ss_exe = doublequote(ss_suf)
+local st_suf = cli_path .. "/fluid-stats"
+local st_exe = doublequote(st_suf)
+------------------------------------------------------------------------------------
+
 local num_selected_items = reaper.CountSelectedMediaItems(0)
 if num_selected_items ~= 0 then
     local captions = "centroid >=:"
@@ -15,31 +26,69 @@ if num_selected_items ~= 0 then
         -- Algorithm Parameters
         local params = commasplit(user_inputs)
         local centroid = params[1]
+        local fftsettings = "1024 -1 -1"
 
         -- Create storage --
-        local item_t = {}
-        local centroid_t = {}
+        local item_t = {} -- table of selected items
+        local ss_cmd_t = {} -- command line arguments for spectralshape
+        local st_cmd_t = {} -- command line arguments for stats
+        local centroid_t = {} -- centroid data   
+        local string_data_t = {} -- all the output data as raw strings   
+        local tmp_file_t = {} -- annoying tmp files made by os.tmpname()
+        local tmp_anal_t = {} -- analysis files made by processor (spectralshape, loudness)
+        local tmp_stat_t = {} -- stats files made by fluid.bufstats~
+        local full_path_t = {} -- full paths to media items
 
         for i=1, num_selected_items do
-
-            -- Pre-processing --
             local tmp_file = os.tmpname()
-            local tmp_idx = tmp_file .. ".csv"
+            local tmp_anal = tmp_file .. "anal" .. ".wav"
+            local tmp_stat = tmp_file .. "stat" .. ".csv"
             table.insert(tmp_file_t, tmp_file)
-            table.insert(tmp_idx_t, tmp_idx)
+            table.insert(tmp_anal_t, tmp_anal)
+            table.insert(tmp_stat_t, tmp_stat)
 
             local item = reaper.GetSelectedMediaItem(0, i-1)
             local take = reaper.GetActiveTake(item)
             local src = reaper.GetMediaItemTake_Source(take)
-            local sr = reaper.GetMediaSourceSampleRate(src)
             local full_path = reaper.GetMediaSourceFileName(src, '')
+            local sr = reaper.GetMediaSourceSampleRate(src)
             table.insert(item_t, item)
+            table.insert(full_path_t, full_path)
 
-            local take_ofs = stosamps(reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS"))
-            local item_pos = stosamps(reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
-            local item_len = stosamps(reaper.GetMediaItemInfo_Value(item, "D_LENGTH"))
+            local take_ofs = stosamps(reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS"), sr)
+            -- local item_pos = stosamps(reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
+            local item_len = stosamps(reaper.GetMediaItemInfo_Value(item, "D_LENGTH"), sr)
 
+            local ss_cmd = ss_exe .. 
+            " -source " .. doublequote(full_path) .. 
+            " -features " .. doublequote(tmp_anal) .. 
+            " -fftsettings " .. fftsettings .. 
+            " -numframes " .. item_len .. 
+            " -startframe " .. take_ofs
+
+            local st_cmd = st_exe .. 
+            " -source " .. doublequote(tmp_anal) .. 
+            " -stats " .. doublequote(tmp_stat) 
+
+            table.insert(ss_cmd_t, ss_cmd)
+            table.insert(st_cmd_t, st_cmd)
         end
+
+        -- Fill the table with data --
+        for i=1, num_selected_items do
+            os.execute(ss_cmd_t[i])
+            os.execute(st_cmd_t[i])
+            table.insert(string_data_t, readfile(tmp_stat_t[i]))
+        end
+        reaper.ShowConsoleMsg(string_data_t[1])
+        -- Read data and execute --
+        for i=1, num_selected_items do
+            remove_file(tmp_file_t[i])
+            remove_file(tmp_anal_t[i])
+            remove_file(tmp_stat_t[i])
+        end
+
+
         reaper.UpdateArrange()
         reaper.Undo_EndBlock("CentroidSelect", 0)
     end
