19ff84b0608b783fe558c7d043eaec5d25f402dc
James Bradbury <jamesbradbury93@gmail.com>
2019-11-22 12:45:41 +0000

    somewhat working...

--- experimental/filter-by-loudness.lua
+++ experimental/filter-by-loudness.lua
@@ -9,34 +9,37 @@ dofile(script_path .. "FluidUtils.lua")
 if sanity_check() == false then goto exit; end
 local cli_path = get_fluid_path()
 --   Then we form some calls to the tools that will live in that folder --
-local ss_suf = cli_path .. "/fluid-spectralshape"
-local ss_exe = doublequote(ss_suf)
+local fl_suf = cli_path .. "/fluid-loudness"
+local fl_exe = doublequote(fl_suf)
 local st_suf = cli_path .. "/fluid-stats"
 local st_exe = doublequote(st_suf)
 ------------------------------------------------------------------------------------
 
 local num_selected_items = reaper.CountSelectedMediaItems(0)
 if num_selected_items ~= 0 then
-    local captions = "operator,centroid,fftsettings"
-    local caption_defaults = ">,500,2048 -1 -1"
-    local confirm, user_inputs = reaper.GetUserInputs("Configuration", 3, captions, caption_defaults)
+    local captions = "operator,amp (dB),windowsize,hopsize,kweighting,truepeak"
+    local caption_defaults = ">,-24,1024,512,1,1"
+    local confirm, user_inputs = reaper.GetUserInputs("Configuration", 6, captions, caption_defaults)
     if confirm then 
         reaper.Undo_BeginBlock()
         -- Algorithm Parameters
         local params = commasplit(user_inputs)
         local operator = params[1]
-        local centroid = tonumber(params[2])
-        local fftsettings = params[3]
-
+        local loudness = tonumber(params[2])
+        local windowsize = params[3]
+        local hopsize = params[4]
+        local kweighting = params[5]
+        local truepeak = params[6]
+        
         -- Create storage --
         local item_t = {} -- table of selected items
-        local ss_cmd_t = {} -- command line arguments for spectralshape
+        local fl_cmd_t = {} -- command line arguments for spectralshape
         local st_cmd_t = {} -- command line arguments for stats
         chans_t = {}
-        centroid_t = {} -- centroid data   
+        loudness_t = {} -- centroid data   
         local string_data_t = {} -- all the output data as raw strings   
         local tmp_file_t = {} -- annoying tmp files made by os.tmpname()
-        local tmp_anal_t = {} -- analysis files made by processor (spectralshape, loudness)
+        local tmp_anal_t = {} -- analysis files made by processor
         local tmp_stat_t = {} -- stats files made by fluid.bufstats~
         local full_path_t = {} -- full paths to media items
         shape_t = {}
@@ -60,27 +63,27 @@ if num_selected_items ~= 0 then
             table.insert(full_path_t, full_path)
 
             local take_ofs = stosamps(reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS"), sr)
-            -- local item_pos = stosamps(reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
             local item_len = stosamps(reaper.GetMediaItemInfo_Value(item, "D_LENGTH"), sr)
 
-            local ss_cmd = ss_exe .. 
+            local fl_cmd = fl_exe .. 
             " -source " .. doublequote(full_path) .. 
             " -features " .. doublequote(tmp_anal) .. 
-            " -fftsettings " .. fftsettings .. 
-            " -numframes " .. item_len .. 
-            " -startframe " .. take_ofs
+            " -windowsize " .. windowsize .. 
+            " -hopsize " .. hopsize ..
+            " -kweighting " .. kweighting ..
+            " -truepeak " .. truepeak
 
             local st_cmd = st_exe .. 
             " -source " .. doublequote(tmp_anal) .. 
             " -stats " .. doublequote(tmp_stat) 
 
-            table.insert(ss_cmd_t, ss_cmd)
+            table.insert(fl_cmd_t, fl_cmd)
             table.insert(st_cmd_t, st_cmd)
         end
 
         -- Fill the table with data --
         for i=1, num_selected_items do
-            os.execute(ss_cmd_t[i])
+            os.execute(fl_cmd_t[i])
             os.execute(st_cmd_t[i])
         end
         
@@ -95,17 +98,14 @@ if num_selected_items ~= 0 then
             -- Take the median of every channel --
             -- if there is more than 1 average the medians --
             local accum = 0
-            for j=1, chans_t[i] do --loop over the amount of channels
-                local lookup = (j * 7) - 6
-                accum = accum + temporary_stats[lookup][6]
-            end
-            accum = accum / chans_t[i]
-            table.insert(centroid_t, accum)
+            -- local lookup = (1 * 7) - 6
+            local lookup = temporary_stats[1][6]
+            table.insert(loudness_t, lookup)
         end
 
         -- Selection logic --
         for i=1, num_selected_items do
-            reaper.SetMediaItemSelected(item_t[i], matchers[operator](centroid_t[i], centroid))
+            reaper.SetMediaItemSelected(item_t[i], matchers[operator](loudness_t[i], loudness))
         end
 
         -- Cleanup --
@@ -116,7 +116,7 @@ if num_selected_items ~= 0 then
         end
 
         reaper.UpdateArrange()
-        reaper.Undo_EndBlock("CentroidSelect", 0)
+        reaper.Undo_EndBlock("LoudnessSelect", 0)
     end
 end
 ::exit::
