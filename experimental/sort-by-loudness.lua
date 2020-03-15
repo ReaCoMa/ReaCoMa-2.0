2b6fa394566bf24bc003927a5375d274149a9c67
James Bradbury <jamesbradbury93@gmail.com>
2020-03-15 18:47:50 +0000

    touch up

--- experimental/sort-by-loudness.lua
+++ experimental/sort-by-loudness.lua
@@ -22,7 +22,7 @@ if num_selected_items > 0 then
     check_params(processor)
     local param_names = "hopsize,windowsize,kweighting,truepeak"
     local param_values = parse_params(param_names, processor)
-    local confirm, user_inputs = reaper.GetUserInputs("Filter by Loudness", 4, param_names, param_values)
+    local confirm, user_inputs = reaper.GetUserInputs("Sort by Loudness", 4, param_names, param_values)
 
     if confirm then
         store_params(processor, param_names, user_inputs)
@@ -86,7 +86,6 @@ if num_selected_items > 0 then
             " -source " .. doublequote(tmp_anal) .. 
             " -stats " .. doublequote(tmp_stat) ..
             " -numderivs " .. "0"
-            DEBUG(fl_cmd)
 
             table.insert(fl_cmd_t, fl_cmd)
             table.insert(st_cmd_t, st_cmd)
@@ -99,41 +98,41 @@ if num_selected_items > 0 then
         end
         
         -- Convert the CSV into a table --
-        -- for i=1, num_selected_items do
-        --     local temporary_stats = {}
+        for i=1, num_selected_items do
+            local temporary_stats = {}
 
-        --     for line in io.lines(tmp_stat_t[i]) do
-        --         table.insert(temporary_stats, statstotable(line))
-        --     end
+            for line in io.lines(tmp_stat_t[i]) do
+                table.insert(temporary_stats, statstotable(line))
+            end
 
-        --     -- Take the median of every channel --
-        --     -- if there is more than 1 average the medians --
-        --     local accum = 0
-        --     -- local lookup = (1 * 7) - 6
-        --     -- TODO Work on multichannel files by averaging medians
-        --     local lookup = temporary_stats[1][6]
-        --     table.insert(loudness_t, tonumber(lookup))
-        -- end
+            -- Take the median of every channel --
+            -- if there is more than 1 average the medians --
+            local accum = 0
+            -- local lookup = (1 * 7) - 6
+            -- TODO Work on multichannel files by averaging medians
+            local lookup = temporary_stats[1][6]
+            table.insert(loudness_t, tonumber(lookup))
+        end
 
-        -- for k, v in Fluid.spairs(loudness_t, function(t,a,b) return t[a] < t[b] end) do
-        --     table.insert(sorted_items, k)
-        -- end
+        for k, v in Fluid.spairs(loudness_t, function(t,a,b) return t[a] < t[b] end) do
+            table.insert(sorted_items, k)
+        end
 
-        -- -- CONTIGUOUS ITEMS MODE --
-        -- -- This can eventually just be merged with the above function
-        -- -- accum_offset = item_pos_t[1]
+        -- CONTIGUOUS ITEMS MODE --
+        -- This can eventually just be merged with the above function
+        accum_offset = item_pos_t[1]
         
-        -- -- for i=1, #sorted_items do
+        for i=1, #sorted_items do
             
-        -- --     local index = sorted_items[i]
-        -- --     local item = item_t[index]
-        -- --     local len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+            local index = sorted_items[i]
+            local item = item_t[index]
+            local len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
             
-        -- --     reaper.SetMediaItemInfo_Value(item, "D_POSITION", accum_offset)
-        -- --     accum_offset = accum_offset + len
-        -- -- end
+            reaper.SetMediaItemInfo_Value(item, "D_POSITION", accum_offset)
+            accum_offset = accum_offset + len
+        end
 
-        -- -- POSITION REPLACEMENT MODE --
+        -- POSITION REPLACEMENT MODE --
         -- for i=1, #sorted_items do
             
         --     local index = sorted_items[i]
@@ -144,9 +143,9 @@ if num_selected_items > 0 then
         -- end
 
 
-        -- cleanup(tmp_file_t)
-        -- cleanup(tmp_anal_t)
-        -- cleanup(tmp_stat_t)
+        cleanup(tmp_file_t)
+        cleanup(tmp_anal_t)
+        cleanup(tmp_stat_t)
 
         reaper.UpdateArrange()
         reaper.Undo_EndBlock("LoudnessSort", 0)
