db88219afb66367dea0cc3f11b241512b7361d54
James Bradbury <jamesbradbury93@gmail.com>
2020-03-16 00:39:55 +0000

    work on auto noveltyslice

--- experimental/fluid-auto-noveltyslice-lr.lua
+++ experimental/fluid-auto-noveltyslice-lr.lua
@@ -6,8 +6,6 @@ dofile(script_path .. "../" .. "FluidParams.lua")
 ------------------------------------------------------------------------------------
 if sanity_check() == false then goto exit; end
 local cli_path = get_fluid_path()
-local ie_suf = cli_path .. "/index_extractor"
-local ie_exe = doublequote(ie_suf)
 local ns_suf = cli_path .. "/noveltyslice"
 local ns_exe = doublequote(ns_suf)
 ------------------------------------------------------------------------------------
@@ -51,6 +49,7 @@ if num_selected_items > 0 then
         local item_len_samples_t = {}
         local slice_points_string_t = {}
         local item_t = {}
+        local tmp_idx_t = {}
         local sr_t = {}
         local take_ofs_t = {}
         local take_ofs_samples_t = {}
@@ -64,6 +63,9 @@ if num_selected_items > 0 then
             table.insert(item_t, item)
             table.insert(sr_t, sr)
             table.insert(full_path_t, full_path)
+
+            local tmp_idx = full_path .. uuid(1) .. "slices" .. ".csv"
+            table.insert(tmp_idx_t, tmp_idx)
             
             local take_ofs = reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
             local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
@@ -83,9 +85,6 @@ if num_selected_items > 0 then
 
         -- Optimisation
         -- Generate a temporary idx file and keep it for the duration of the optimisation
-        local tmp_bse = os.tmpname()
-        local tmp_idx = doublequote(tmp_bse .. ".wav")
-        local read_cmd = ie_exe .. " " .. tmp_idx
         for i=1, num_selected_items do
             -- Set up some values in memory
             local max_iter = 1000
@@ -104,6 +103,7 @@ if num_selected_items > 0 then
             noveltyslice(full_path_t[i], tmp_idx, feature, tostring(curr_thresh), kernelsize, filtersize, fftsettings)
             curr_slices = tablelen(spacesplit(capture(read_cmd, false)))
 
+
             -- start searching --
             while iter ~= max_iter do
                 if num_slices == target_slices then -- if it is already solved
@@ -111,12 +111,14 @@ if num_selected_items > 0 then
                     goto finish_search;
                 end
 
-                delta_slices = 
-
                 if num_slices > target_slices then 
                     curr_thresh = curr_thresh * (1.23 + (math.random() * 0.05))
                     noveltyslice(full_path_t[i], tmp_idx, feature, tostring(curr_thresh), kernelsize, filtersize, fftsettings)
                     num_slices = tablelen(spacesplit(capture(read_cmd, false)))
+                    for line in io.lines(tmp_idx) do
+                        DEBUG(line)
+                        table.insert(temporary_stats, statstotable(line))
+                    end
                 end
 
                 if num_slices < target_slices then
