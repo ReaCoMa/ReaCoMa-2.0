a0f06942ca3a4554a6b27d0809d80983d55c45d5
James Bradbury <jamesbradbury93@gmail.com>
2020-03-27 11:48:22 +0000

    correct pathing

--- utils/wipe_states.lua
+++ utils/wipe_states.lua
@@ -1,12 +1,12 @@
 local info = debug.getinfo(1,'S');
 local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
-dofile(script_path .. "/FluidPlumbing/" .. "FluidParams.lua")
-dofile(script_path .. "/FluidPlumbing/" .. "FluidUtils.lua")
+dofile(script_path .. "../FluidPlumbing/" .. "FluidParams.lua")
+dofile(script_path .. "../FluidPlumbing/" .. "FluidUtils.lua")
 
+
+-- A script for getting rid of all FluCoMa related variables stored in the state table --
 for k, v in pairs(fluid_archetype) do
-    DEBUG(v.name)
     for i, j in pairs(v) do
-        DEBUG(i)
         reaper.DeleteExtState(v.name, i, true)
     end
 end
