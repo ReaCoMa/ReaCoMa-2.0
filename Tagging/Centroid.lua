883b5e3d7b78d71daf3895172bbf96eccc68e3f4
James Bradbury <jamesbradbury93@gmail.com>
2020-04-01 17:13:24 +0100

    move to new namespaced functions

--- Tagging/Centroid.lua
+++ Tagging/Centroid.lua
@@ -4,26 +4,26 @@ dofile(script_path .. "../FluidPlumbing/" .. "fluidUtils.lua")
 dofile(script_path .. "../FluidPlumbing/" .. "fluidParams.lua")
 dofile(script_path .. "../FluidPlumbing/" .. "fluidTagging.lua")
 
-if sanity_check() == false then goto exit; end
-local loudness_exe = doublequote(get_fluid_path() .. "/fluid-spectralshape")
-local stats_exe = doublequote(get_fluid_path() .. "/fluid-stats")
+if fluidPaths.sanity_check() == false then goto exit; end
+local loudness_exe = fluidUtils.doublequote(fluidPaths.get_fluid_path() .. "/fluid-spectralshape")
+local stats_exe = fluidUtils.doublequote(fluidPaths.get_fluid_path() .. "/fluid-stats")
 
 local num_selected_items = reaper.CountSelectedMediaItems(0)
     if num_selected_items > 0 then
-        data = TaggingContainer
+        local data = fluidTagging.container
 
         for i=1, num_selected_items do
-            get_tag_data(i, data)
+            fluidTagging.get_data(i, data)
             
             local analcmd = loudness_exe ..
-            " -source " .. doublequote(data.full_path[i]) ..
-            " -features " .. doublequote(data.analtmp[i]) ..
+            " -source " .. fluidUtils.doublequote(data.full_path[i]) ..
+            " -features " .. fluidUtils.doublequote(data.analtmp[i]) ..
             " -fftsettings " .. "4096 1024 4096"
             table.insert(data.analcmd, analcmd)
             
             local statscmd = stats_exe ..
-            " -source " .. doublequote(data.analtmp[i]) ..
-            " -stats " .. doublequote(data.statstmp[i])
+            " -source " .. fluidUtils.doublequote(data.analtmp[i]) ..
+            " -stats " .. fluidUtils.doublequote(data.statstmp[i])
             table.insert(data.statscmd, statscmd)
         end
 
@@ -32,21 +32,21 @@ local num_selected_items = reaper.CountSelectedMediaItems(0)
             cmdline(data.statscmd[i])
 
             local channel1 = linesplit(
-                readfile(data.statstmp[i])
+                fluidUtils.readfile(data.statstmp[i])
             )[1]
 
-            local analysis_data = commasplit(channel1) -- whatver your numbers are basically
+            local analysis_data = fluidUtils.commasplit(channel1) -- whatver your numbers are basically
 
-            update_notes(data.item[i], "-- Centroid Analysis --")
+            fluidTagging.update_notes(data.item[i], "-- Centroid Analysis --")
             local details = "Average: " .. analysis_data[1] .. "\r\n" ..
             "Min: " .. analysis_data[5] .. "\r\n" ..
             "Max: " .. analysis_data[7] .. "\r\n" ..
             "Median: " .. analysis_data[6] .. "\r\n"
-            update_notes(data.item[i], details)
+            fluidTagging.update_notes(data.item[i], details)
         end
         reaper.UpdateArrange()
         reaper.Undo_EndBlock("TagLoudness", 0)
-        cleanup(data.analtmp)
-        cleanup(data.statstmp)
+        fluidUtils.cleanup(data.analtmp)
+        fluidUtils.cleanup(data.statstmp)
     end
 ::exit::
--- Tagging/Loudness.lua
+++ Tagging/Loudness.lua
@@ -4,27 +4,27 @@ dofile(script_path .. "../FluidPlumbing/" .. "fluidUtils.lua")
 dofile(script_path .. "../FluidPlumbing/" .. "fluidParams.lua")
 dofile(script_path .. "../FluidPlumbing/" .. "fluidTagging.lua")
 
-if sanity_check() == false then goto exit; end
-local anal_exe = doublequote(get_fluid_path() .. "/fluid-loudness")
-local stats_exe = doublequote(get_fluid_path() .. "/fluid-stats")
+if fluidPaths.sanity_check() == false then goto exit; end
+local anal_exe = fluidUtils.doublequote(fluidPaths.get_fluid_path() .. "/fluid-loudness")
+local stats_exe = fluidUtils.doublequote(fluidPaths.get_fluid_path() .. "/fluid-stats")
 
 local num_selected_items = reaper.CountSelectedMediaItems(0)
     if num_selected_items > 0 then
-        data = TaggingContainer
+        local data = fluidTagging.container
 
         for i=1, num_selected_items do
-            get_tag_data(i, data)
+            fluidTagging.get_data(i, data)
             
             local analcmd = anal_exe ..
-            " -source " .. doublequote(data.full_path[i]) ..
-            " -features " .. doublequote(data.analtmp[i]) ..
+            " -source " .. fluidUtils.doublequote(data.full_path[i]) ..
+            " -features " .. fluidUtils.doublequote(data.analtmp[i]) ..
             " -windowsize " .. "17640" ..
             " -hopsize " .. "4410"
             table.insert(data.analcmd, analcmd)
             
             local statscmd = stats_exe ..
-            " -source " .. doublequote(data.analtmp[i]) ..
-            " -stats " .. doublequote(data.statstmp[i])
+            " -source " .. fluidUtils.doublequote(data.analtmp[i]) ..
+            " -stats " .. fluidUtils.doublequote(data.statstmp[i])
             table.insert(data.statscmd, statscmd)
         end
 
@@ -33,21 +33,21 @@ local num_selected_items = reaper.CountSelectedMediaItems(0)
             cmdline(data.statscmd[i])
 
             local channel1 = linesplit(
-                readfile(data.statstmp[i])
+                fluidUtils.readfile(data.statstmp[i])
             )[1]
 
-            local analysis_data = commasplit(channel1) -- whatver your numbers are basically
+            local analysis_data = fluidUtils.commasplit(channel1) -- whatver your numbers are basically
 
-            update_notes(data.item[i], "-- Loudness Analysis --")
+            fluidTagging.update_notes(data.item[i], "-- Loudness Analysis --")
             local details = "Average: " .. analysis_data[1] .. "\r\n" ..
             "Min: " .. analysis_data[5] .. "\r\n" ..
             "Max: " .. analysis_data[7] .. "\r\n" ..
             "Median: " .. analysis_data[6] .. "\r\n"
-            update_notes(data.item[i], details)
+            fluidTagging.update_notes(data.item[i], details)
         end
         reaper.UpdateArrange()
         reaper.Undo_EndBlock("TagLoudness", 0)
-        cleanup(data.analtmp)
-        cleanup(data.statstmp)
+        fluidUtils.cleanup(data.analtmp)
+        fluidUtils.cleanup(data.statstmp)
     end
 ::exit::
