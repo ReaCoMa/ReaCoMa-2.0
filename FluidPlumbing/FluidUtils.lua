7ae355f84fc87c3d77315d65c237ff055dab01f7
James Bradbury <jamesbradbury93@gmail.com>
2020-04-01 12:21:43 +0100

    namespace utilities to FluidUtils

--- FluidPlumbing/FluidUtils.lua
+++ FluidPlumbing/FluidUtils.lua
@@ -1,51 +1,51 @@
 FluidUtils = {}
 
-function DEBUG(string)
+FluidUtils.DEBUG = function(string)
     reaper.ShowConsoleMsg(string)
     reaper.ShowConsoleMsg("\n")
 end
 
-function uuid(idx)
+FluidUtils.uuid = function(idx)
     local time = tostring(reaper.time_precise()):gsub("%.+", "")
     return time .. idx
 end
 
-function cmdline(string)
+FluidUtils.cmdline = function(string)
     local opsys = reaper.GetOS()
     if opsys == "Win64" then reaper.ExecProcess(string, 0) end
     if opsys == "OSX64" or opsys == "Other" then os.execute(string) end
 end
 
-function sampstos(samps_in, sr)
+FluidUtils.sampstos = function(samps_in, sr)
     return samps_in / sr
 end
 
-function stosamps(secs_in, sr) 
+FluidUtils.stosamps = function(secs_in, sr) 
     return math.floor((secs_in * sr) + 0.5)
 end
 
-function basedir(str,sep)
+FluidUtils.basedir = function(str,sep)
     sep=sep or'/'
     return str:match("(.*"..sep..")")
 end
 
-function basename(input_string)
+FluidUtils.basename = function(input_string)
     return input_string:match("(.+)%..+")
 end
 
-function rm_trailing_slash(s)
+FluidUtils.rm_trailing_slash = function(s)
     -- Remove trailing slash from string. 
     -- Will not remove slash if it is the only character.
     return s:gsub('(.)%/$', '%1')
 end
 
-function cleanup(path_table)
+FluidUtils.cleanup = function(path_table)
     for i=1, #path_table do
         os.remove(path_table[i])
     end
 end
 
-function capture(cmd, raw)
+FluidUtils.capture = function(cmd, raw)
     -- usage: local output = capture("ls", false)
     local f = assert(io.popen(cmd, 'r'))
     local s = assert(f:read('*a'))
@@ -57,14 +57,14 @@ function capture(cmd, raw)
     return s
 end
 
-function readfile(file)
+FluidUtils.readfile = function(file)
     local f = assert(io.open(file, "r"))
     local content = f:read("*all")
     f:close()
     return content
 end
 
-function commasplit(input_string)
+FluidUtils.commasplit = function(input_string)
     -- splits by ,
     local t = {}
     for word in string.gmatch(input_string, '([^,]+)') do
@@ -73,7 +73,7 @@ function commasplit(input_string)
     return t
 end
 
-function linesplit(input_string)
+FluidUtils.linesplit = function(input_string)
     -- splits by line endings
     local t = {}
     for word in string.gmatch(input_string,"(.-)\r?\n") do
@@ -82,7 +82,7 @@ function linesplit(input_string)
     return t
 end
 
-function lacetables(table1, table2)
+FluidUtils.lacetables = function(table1, table2)
     laced = {}
     for i=1, #table1 do
         table.insert(laced, table1[i])
@@ -91,7 +91,7 @@ function lacetables(table1, table2)
     return laced
 end
 
-function statstotable(string)
+FluidUtils.statstotable = function(string)
     local t = {}
     for word in string:gmatch('([^,]+)') do
         table.insert(t, tonumber(word))
@@ -99,25 +99,25 @@ function statstotable(string)
     return t
 end
 
-function spacesplit(input_string)
+FluidUtils.spacesplit = function(input_string)
     local t = {}
     for word in input_string:gmatch("%w+") do table.insert(t, word) end
     return t
 end
 
-function rmdelim(input_string)
+FluidUtils.rmdelim = function(input_string)
     local nodots = input_string.gsub(input_string, "%.", "")
     local nospace = nodots.gsub(nodots, "%s", "")
     return nospace
 end
 
-function tablelen(t)
+FluidUtils.tablelen = function(t)
   local count = 0
   for _ in pairs(t) do count = count + 1 end
   return count
 end
 
-function doublequote(input_string)
+FluidiUtils.doublequote = function(input_string)
     return '"'..input_string..'"'
 end
 
