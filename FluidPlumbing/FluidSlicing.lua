f3001aa370957a27f7bef80a0033e369f17362eb
James Bradbury <jamesbradbury93@gmail.com>
2020-04-03 18:10:36 +0100

    slicing on all permutations of item manipulation

--- FluidPlumbing/FluidSlicing.lua
+++ FluidPlumbing/FluidSlicing.lua
@@ -39,10 +39,12 @@ fluidSlicing.get_data = function (item_index, data)
     end
     
     local tmp = full_path .. fluidUtils.uuid(item_index) .. "fs.csv"
+    local playrate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
     local take_ofs = reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
     local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    local playrate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
-    
+    local src_len = reaper.GetMediaSourceLength(src)
+    local item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH") * playrate
+
     if data.reverse[item_index] then
         take_ofs = math.abs(src_len - (item_len + take_ofs))
     end
@@ -54,7 +56,7 @@ fluidSlicing.get_data = function (item_index, data)
 
     local take_ofs_samples = fluidUtils.stosamps(take_ofs, sr)
     local item_pos_samples = fluidUtils.stosamps(item_pos, sr)
-    local item_len_samples = math.floor(fluidUtils.stosamps(item_len, sr) * playrate)
+    local item_len_samples = math.floor(fluidUtils.stosamps(item_len, sr))
 
     table.insert(data.item, item)
     table.insert(data.sr, sr)
@@ -69,16 +71,36 @@ fluidSlicing.get_data = function (item_index, data)
 end
 
 fluidSlicing.perform_splitting = function (item_index, data)
+    -- Thank you to Francesco Cameli for helping me debug this absolute NIGHTMARE --
     local slice_points = fluidUtils.commasplit(data.slice_points_string[item_index])
+
+    -- Invert the table around the middle point (mirror!)
+    if data.reverse[item_index] then
+        local half_length = (data.item_len_samples[item_index]) * 0.5
+        for i=1, #slice_points do
+            slice_points[i] = half_length + (half_length - slice_points[i])
+        end
+        fluidUtils.reversetable(slice_points)
+    end
+    
+    -- if the left boundary is the start remove it
     if tonumber(slice_points[1]) == data.take_ofs_samples[item_index] then table.remove(slice_points, 1) end
 
+    -- now sanitise the numbers to adjust for the take offset and playback rate
+    for i=1, #slice_points do
+        if data.reverse[item_index] then
+            slice_points[i] = (slice_points[i] + data.take_ofs_samples[item_index]) * (1 / data.playrate[item_index])
+        else
+            slice_points[i] = (slice_points[i] - data.take_ofs_samples[item_index]) * (1 / data.playrate[item_index])
+        end
+    end
+
     for j=1, #slice_points do
-        local slice_index = j
         slice_pos = fluidUtils.sampstos(
-            tonumber(slice_points[slice_index]), 
+            tonumber(slice_points[j]), 
             data.sr[item_index]
         )
-        slice_pos = (data.item_pos[item_index] + ((slice_pos - data.take_ofs[item_index]) * (1 / data.playrate[item_index])))  -- account for playback rate
+        slice_pos = data.item_pos[item_index] + slice_pos
         data.item[item_index] = reaper.SplitMediaItem(
             data.item[item_index], 
             slice_pos
@@ -90,17 +112,34 @@ fluidSlicing.perform_gate_splitting = function(item_index, data, init_state)
     local state = init_state
     local slice_points = fluidUtils.commasplit(data.slice_points_string[item_index])
 
+    -- Invert the table around the middle point (mirror!)
+    if data.reverse[item_index] then
+        local half_length = (data.item_len_samples[item_index]) * 0.5
+        for i=1, #slice_points do
+            slice_points[i] = half_length + (half_length - slice_points[i])
+        end
+        fluidUtils.reversetable(slice_points)
+    end
+
+    -- if the left boundary is the start remove it
+    if tonumber(slice_points[1]) == data.take_ofs_samples[item_index] then table.remove(slice_points, 1) end
+
+    -- now sanitise the numbers to adjust for the take offset and playback rate
+    for i=1, #slice_points do
+        if data.reverse[item_index] then
+            slice_points[i] = (slice_points[i] + data.take_ofs_samples[item_index]) * (1 / data.playrate[item_index])
+        else
+            slice_points[i] = (slice_points[i] - data.take_ofs_samples[item_index]) * (1 / data.playrate[item_index])
+        end
+    end
+
     for j=1, #slice_points do
-
-        if tonumber(slice_points[1]) == data.take_ofs_samples[item_index] then table.remove(slice_points, 1) end
-
-        local slice_index = j
         slice_pos = fluidUtils.sampstos(
-            tonumber(slice_points[slice_index]), 
+            tonumber(slice_points[j]), 
             data.sr[item_index]
         )
         
-        slice_pos = (data.item_pos[item_index] + ((slice_pos - data.take_ofs[item_index]) * (1 / data.playrate[item_index])))  -- account for playback rate
+        slice_pos = data.item_pos[item_index] + slice_pos  -- account for playback rate
         reaper.SetMediaItemInfo_Value(data.item[item_index], "B_MUTE", state)
         data.item[item_index] = reaper.SplitMediaItem(
             data.item[item_index], 
