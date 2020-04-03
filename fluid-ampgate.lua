7aa70a92ab05d966731fc7d44b7c926e91022c69
James Bradbury <jamesbradbury93@gmail.com>
2020-04-03 14:05:45 +0100

    change state according to the start sample

--- fluid-ampgate.lua
+++ fluid-ampgate.lua
@@ -10,7 +10,6 @@ local exe = fluidUtils.doublequote(fluidPaths.get_fluid_path() .. "/fluid-ampgat
 
 local num_selected_items = reaper.CountSelectedMediaItems(0)
 if num_selected_items > 0 then
-
     local processor = fluid_archetype.ampgate
     fluidParams.check_params(processor)
     local param_names = "rampup,rampdown,onthreshold,offthreshold,minslicelength,minsilencelength,minlengthabove,minlengthbelow,lookback,lookahead,highpassfreq,maxsize"
@@ -19,7 +18,7 @@ if num_selected_items > 0 then
     local confirm, user_inputs = reaper.GetUserInputs("Ampgate Parameters", 12, param_names, param_values)
     if confirm then
         fluidParams.store_params(processor, param_names, user_inputs)
-
+        
         reaper.Undo_BeginBlock()
         local params = fluidUtils.commasplit(user_inputs)
         local rampup = params[1]
@@ -67,17 +66,17 @@ if num_selected_items > 0 then
             onsets = fluidUtils.commasplit(channel_split[1])
             offsets = fluidUtils.commasplit(channel_split[2])
             laced = fluidUtils.lacetables(onsets, offsets)
-            if #laced <= 2 then goto exit; end
             dumb_string = ""
             local state_state = nil
-            if laced[1] == 0 then 
+            
+            if laced[1] == data.take_ofs_samples[i] then 
                 start_state = 0 -- if there is a 0 at the start we start 'off/unmuted'
             else 
-                table.insert(laced, 1, "0")
                 start_state = 1 -- if there is something else at the start we start muted and prepend a 0
             end
-            for i=1, #laced do
-                dumb_string = dumb_string .. laced[i] .. ","
+
+            for j=1, #laced do
+                dumb_string = dumb_string .. laced[j] .. ","
             end
             table.insert(data.slice_points_string, dumb_string)
             fluidSlicing.perform_gate_splitting(i, data, start_state)
