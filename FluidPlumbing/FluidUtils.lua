3607e67987fe8e472353bec79a4d44b86ec56540
James Bradbury <jamesbradbury93@gmail.com>
2020-04-12 17:28:25 +0100

    add a table sorting function

--- FluidPlumbing/FluidUtils.lua
+++ FluidPlumbing/FluidUtils.lua
@@ -5,6 +5,29 @@ fluidUtils.DEBUG = function(string)
     reaper.ShowConsoleMsg("\n")
 end
 
+fluidUtils.spairs = function(t, order)
+    -- collect the keys
+    local keys = {}
+    for k in pairs(t) do keys[#keys+1] = k end
+
+    -- if order function given, sort by it by passing the table and keys a, b,
+    -- otherwise just sort the keys 
+    if order then
+        table.sort(keys, function(a,b) return order(t, a, b) end)
+    else
+        table.sort(keys)
+    end
+
+    -- return the iterator function
+    local i = 0
+    return function()
+        i = i + 1
+        if keys[i] then
+            return keys[i], t[keys[i]]
+        end
+    end
+end
+
 fluidUtils.nextpowstr = function(x)
     return tostring(
         math.floor(2^math.ceil(math.log(x)/math.log(2)))
