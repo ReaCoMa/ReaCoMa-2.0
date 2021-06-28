1a70b09f24e88325999b03c17bf1c1ba6bfb5022
James Bradbury <jamesbradbury93@gmail.com>
2021-06-28 22:38:31 +0100

    genericise the wrapper for layers

--- lib/imgui_wrapper.lua
+++ lib/imgui_wrapper.lua
@@ -1,11 +1,11 @@
 imgui_wrapper = {}
 
 -- FRAME LOOP --
-imgui_wrapper.loop = function(ctx, viewport, state, slicer, preview)
+imgui_wrapper.loop = function(ctx, viewport, state, obj, preview)
     if reaper.ImGui_IsCloseRequested(ctx) then
         reaper.ImGui_DestroyContext(ctx)
-        reaper.Undo_EndBlock2(0, 'dynamic-slicing', 4)
-        reacoma.params.set(slicer)
+        reaper.Undo_EndBlock2(0, slicer.info.ext_name, 4)
+        reacoma.params.set(obj)
         return
     end
     
@@ -14,19 +14,19 @@ imgui_wrapper.loop = function(ctx, viewport, state, slicer, preview)
     reaper.ImGui_Begin(ctx, 'wnd', nil, reaper.ImGui_WindowFlags_NoDecoration())
 
     ---------------------------------  FRAME  -------------------------
-    if reaper.ImGui_Button(ctx, 'segment') then
-        state = reacoma.imgui_helpers.button_segment(slicer)
+    if reaper.ImGui_Button(ctx, obj.info.action) then
+        state = reacoma.imgui_helpers.button_segment(obj)
     end
     
     reaper.ImGui_SameLine(ctx)
     _, preview = reaper.ImGui_Checkbox(ctx, 'preview', preview)
-    state = reacoma.imgui_helpers.update_slicing(slicer, preview)
+    state = reacoma.imgui_helpers.slice(obj, preview)
     -------------------------------------------------------------------
 
     reaper.ImGui_End(ctx)
     reaper.defer(
         function() 
-            imgui_wrapper.loop(ctx, viewport, state, slicer, preview) 
+            imgui_wrapper.loop(ctx, viewport, state, obj, preview) 
         end
     )
 end
