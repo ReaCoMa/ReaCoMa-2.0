bf49a6e2de46b7bf2f060ed533df7d5fa1704702
James Bradbury <jamesbradbury93@gmail.com>
2021-06-29 13:27:02 +0100

    begin work supplanting old algorithms

--- fluid-noveltyslice.lua
+++ fluid-noveltyslice.lua
@@ -1,63 +1,17 @@
 local info = debug.getinfo(1,'S');
 local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 loadfile(script_path .. "lib/reacoma.lua")()
-
 if reacoma.settings.fatal then return end
 
-local exe = reacoma.utils.wrap_quotes(
-    reacoma.settings.path .. "/fluid-noveltyslice"
-)
+obj = reacoma.noveltyslice
+parameters = obj.parameters
+reacoma.params.get(obj)
 
-local num_selected_items = reaper.CountSelectedMediaItems(0)
-if num_selected_items > 0 then
+ctx = reaper.ImGui_CreateContext(obj.info.algorithm_name, 350, 225)
+viewport = reaper.ImGui_GetMainViewport(ctx)
 
-    local processor = reacoma.params.archetype.noveltyslice
-    reacoma.params.check_params(processor)
-    local param_names = "feature,threshold,kernelsize,filtersize,fftsettings,minslicelength"
-    local param_values = reacoma.params.parse_params(param_names, processor)
-
-    local confirm, user_inputs = reaper.GetUserInputs("Noveltyslice Parameters", 6, param_names, param_values)
-    if confirm then
-        reacoma.params.store_params(processor, param_names, user_inputs)
-
-        local params = reacoma.utils.split_comma(user_inputs)
-        local feature = params[1]
-        local threshold = params[2]
-        local kernelsize = params[3]
-        local filtersize = params[4]
-        local fftsettings = params[5]
-        local minslicelength = params[6]
-        
-        local data = reacoma.slicing.container
-
-        for i=1, num_selected_items do
-            reacoma.slicing.get_data(i, data)
-            
-            local cmd = exe .. 
-            " -source " .. reacoma.utils.wrap_quotes(data.full_path[i]) .. 
-            " -indices " .. reacoma.utils.wrap_quotes(data.tmp[i]) .. 
-            " -maxfftsize " .. reacoma.utils.get_max_fft_size(fftsettings) ..
-            " -maxkernelsize " .. kernelsize ..
-            " -maxfiltersize " .. filtersize ..
-            " -feature " .. feature .. 
-            " -kernelsize " .. kernelsize .. 
-            " -threshold " .. threshold .. 
-            " -filtersize " .. filtersize .. 
-            " -fftsettings " .. fftsettings .. 
-            " -minslicelength " .. minslicelength ..
-            " -numframes " .. data.item_len_samples[i] .. 
-            " -startframe " .. data.take_ofs_samples[i]
-            table.insert(data.cmd, cmd)
-        end
-
-        for i=1, num_selected_items do
-            reacoma.utils.cmdline(data.cmd[i])
-            table.insert(data.slice_points_string, reacoma.utils.readfile(data.tmp[i]))
-            reacoma.slicing.process(i, data)
-        end
-
-        reacoma.utils.arrange("reacoma-noveltyslice")
-        reacoma.utils.cleanup(data.tmp)
+reaper.defer(
+    function()
+        reacoma.imgui_wrapper.loop(ctx, viewport, state, obj, preview)
     end
-end
-
+)
