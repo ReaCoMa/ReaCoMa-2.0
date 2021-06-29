c890b6f2792a2e2e9be5979c4230cedd80ca2fb9
James Bradbury <jamesbradbury93@gmail.com>
2021-06-29 13:24:41 +0100

    hpss algo implementation

--- lib/algos/hpss.lua
+++ lib/algos/hpss.lua
@@ -4,23 +4,65 @@ function decompose(parameters)
     )
 
     local num_selected_items = reaper.CountSelectedMediaItems(0)
+    local hfs = parameters[1].value
+    local pfs = parameters[2].value
+    local fftsettings = reacoma.utils.form_fft_string(
+        parameters[3].value,
+        parameters[4].value,
+        parameters[5].value
+    )
 
+    local maskingmode = '0'
+
+    local data = reacoma.utils.deep_copy(reacoma.container.generic)
+    data.outputs = {
+        harmonic = {},
+        percussive = {}
+    }
+
+    for i=1, num_selected_items do
+        reacoma.container.get_data(i, data)
+
+        table.insert(
+            data.outputs.harmonic,
+            data.path[i] .. "_hpss-h_" .. reacoma.utils.uuid(i) .. ".wav"
+        )
+        table.insert(
+            data.outputs.percussive,
+            data.path[i] .. "_hpss-p_" .. reacoma.utils.uuid(i) .. ".wav"
+        )
+
+        table.insert(
+            data.cmd, 
+            exe .. 
+            " -source " .. reacoma.utils.wrap_quotes(data.full_path[i]) .. 
+            " -harmonic " .. reacoma.utils.wrap_quotes(data.outputs.harmonic[i]) .. 
+            " -maxfftsize " .. reacoma.utils.get_max_fft_size(fftsettings) ..
+            " -maxharmfiltersize " .. hfs ..
+            " -maxpercfiltersize " .. pfs ..
+            " -percussive " .. reacoma.utils.wrap_quotes(data.outputs.percussive[i]) ..  
+            " -harmfiltersize " .. hfs .. 
+            " -percfiltersize " .. pfs .. 
+            " -maskingmode " .. maskingmode ..
+            " -fftsettings " .. fftsettings .. 
+            " -numframes " .. data.item_len_samples[i] .. 
+            " -startframe " .. data.take_ofs_samples[i]
+        )
+        reacoma.utils.cmdline(data.cmd[i])
+        reacoma.layers.exist(i, data)
+        reaper.SelectAllMediaItems(0, 0)
+        reacoma.layers.process(i, data)
+        reaper.UpdateArrange()
+    end
 end
 
 noveltyslice = {
     info = {
-        algorithm_name = 'Novelty Slice',
-        ext_name = 'reacoma.noveltyslice',
-        action = 'segment'
+        algorithm_name = 'Harmonic Percussive Source Separation',
+        ext_name = 'reacoma.hpss',
+        action = 'decompose'
     },
     parameters =  {
-        -- {
-        --     name = 'masking mode',
-        --     widget = reaper.ImGui_Combo,
-        --     value = 0,
-        --     items = 'classic\31coupled\31advanced\31',
-        --     type = 'combo' 
-        -- },
         {
             name = 'harmonic filter size',
             widget = reaper.ImGui_SliderInt,
