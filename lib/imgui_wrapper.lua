a0acc7771ff9163d8f3439e00feb0dbcfc8c1eba
James Bradbury <jamesbradbury93@gmail.com>
2021-06-29 13:25:10 +0100

    genercisei info.ext_name call

--- lib/imgui_wrapper.lua
+++ lib/imgui_wrapper.lua
@@ -4,7 +4,7 @@ imgui_wrapper = {}
 imgui_wrapper.loop = function(ctx, viewport, state, obj, preview)
     if reaper.ImGui_IsCloseRequested(ctx) then
         reaper.ImGui_DestroyContext(ctx)
-        reaper.Undo_EndBlock2(0, slicer.info.ext_name, 4)
+        reaper.Undo_EndBlock2(0, obj.info.ext_name, 4)
         reacoma.params.set(obj)
         return
     end
@@ -13,6 +13,7 @@ imgui_wrapper.loop = function(ctx, viewport, state, obj, preview)
     reaper.ImGui_SetNextWindowSize(ctx, reaper.ImGui_Viewport_GetSize(viewport))
     reaper.ImGui_Begin(ctx, 'wnd', nil, reaper.ImGui_WindowFlags_NoDecoration())
 
+    w, h = reaper.ImGui_Viewport_GetSize(viewport)
     ---------------------------------  FRAME  -------------------------
     -- If you touch the process button, or press enter
     if reaper.ImGui_Button(ctx, obj.info.action) or reaper.ImGui_IsKeyPressed(ctx, 13) then
