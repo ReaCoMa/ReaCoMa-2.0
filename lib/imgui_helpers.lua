d762c25073014e4432dd1d7ea6215dc860841b36
James Bradbury <jamesbradbury93@gmail.com>
2021-06-29 12:32:24 +0100

    further genericise the imgui_helper

--- lib/imgui_helpers.lua
+++ lib/imgui_helpers.lua
@@ -30,36 +30,42 @@ imgui_helpers.draw_gui = function(obj)
     return change
 end
 
-imgui_helpers.slice = function(obj, preview)
+imgui_helpers.update_state = function(obj, preview)
+    -- TODO: this could possibly be refactored into a single if statement but for now lets keep it verbose
+    -- Updates the state within each frame loop
     local change = imgui_helpers.draw_gui(obj)
 
-    if change > 0 and preview then
-        return obj.slice(obj.parameters)
-    end
-end
-
-imgui_helpers.layers = function(obj)
-end
-
-
-imgui_helpers.button_segment = function(slicer)
-    local state = slicer.slice(slicer.parameters)
-
-    local num_selected_items = reaper.CountSelectedMediaItems(0)
-    for i=1, num_selected_items do
-        item = reaper.GetSelectedMediaItem(0, i-1)
-        take = reaper.GetActiveTake(item)
-        take_markers = reaper.GetNumTakeMarkers(take)
-
-        for j=1, take_markers do
-            local slice_pos, _, _ = reaper.GetTakeMarker(take, j-1)
-            item = reaper.SplitMediaItem(
-                item, 
-                slice_pos + state.item_pos[i]
-            )
+    -- We only need to update the state intermittently if the object is for segmenting...
+    -- ... and if the preview is checked
+    if obj.info.action == 'segment' then
+        -- Perform a slices process
+        if change > 0 and preview then
+            return obj.perform_update(obj.parameters)
         end
+    end 
+end
+
+imgui_helpers.process = function(obj)
+    local state = obj.perform_update(obj.parameters)
+
+    if obj.info.action == 'segment' then
+        local num_selected_items = reaper.CountSelectedMediaItems(0)
+        for i=1, num_selected_items do
+            item = reaper.GetSelectedMediaItem(0, i-1)
+            take = reaper.GetActiveTake(item)
+            take_markers = reaper.GetNumTakeMarkers(take)
+
+            for j=1, take_markers do
+                local slice_pos, _, _ = reaper.GetTakeMarker(take, j-1)
+                item = reaper.SplitMediaItem(
+                    item, 
+                    slice_pos + state.item_pos[i]
+                )
+            end
+        end
+        reaper.UpdateArrange()
     end
-    reaper.UpdateArrange()
+
     return state
 end
 
