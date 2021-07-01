ea841c0efad0e0bbd534b1d81ccd1ffc48233446
James Bradbury <jamesbradbury93@gmail.com>
2021-07-01 19:11:27 +0100

    deal with enter filtering if something is active

--- lib/imgui_helpers.lua
+++ lib/imgui_helpers.lua
@@ -26,6 +26,7 @@ imgui_helpers.HelpMarker = function(ctx, desc)
 
 imgui_helpers.draw_gui = function(ctx, obj)
     local change = 0
+    local active = 0
     for parameter, d in pairs(obj.parameters) do
         if d.type == 'sliderint' then
             temp, d.value = d.widget(
@@ -47,32 +48,31 @@ imgui_helpers.draw_gui = function(ctx, obj)
                 d.name, d.value, d.items
             )
         end
-
+        local widget_active = reaper.ImGui_IsItemActive(ctx)
         -- Draw the mouseover description
         local help_text = d.desc or 'no help available'
         imgui_helpers.HelpMarker(ctx, help_text)
-
+        active = active + reacoma.utils.bool_to_number[widget_active]
         change = change + reacoma.utils.bool_to_number[temp]
     end
+    reacoma.global_state.active = active
     return change
 end
 
-imgui_helpers.update_state = function(ctx, obj, preview)
+imgui_helpers.update_state = function(ctx, obj)
     -- TODO: this could possibly be refactored into a single if statement but for now lets keep it verbose
     -- Updates the state within each frame loop
-    local change = imgui_helpers.draw_gui(ctx, obj)
-
+    local change, active = imgui_helpers.draw_gui(ctx, obj)
     -- We only need to update the state intermittently if the object is for segmenting...
     -- ... and if the preview is checked
-    if obj.info.action == 'segment' then
-        -- Perform a slices process
-        if change > 0 and preview then
-            return obj.perform_update(obj.parameters)
-        end
+    if obj.info.action == 'segment' and change > 0 and reacoma.settings.slice_preview then
+        return obj.perform_update(obj.parameters)
     end 
 end
 
 imgui_helpers.process = function(obj)
+    -- This is called everytime there is a process buttno process
+    -- This button is uniform across layers/slices and is found at the top left
     local state = obj.perform_update(obj.parameters)
 
     if obj.info.action == 'segment' then
