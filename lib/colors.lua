58ff66f7cd76b8e157f78865d9df5d88ca01dd17
James Bradbury <jamesbradbury93@gmail.com>
2021-07-01 17:35:12 +0100

    store colours separately

---
+++ lib/colors.lua
@@ -0,0 +1,14 @@
+colors = {}
+
+colors.red = reaper.ImGui_ColorConvertHSVtoRGB(0.0, 0.7, 1.0, 1.0)
+
+colors.green = reaper.ImGui_ColorConvertHSVtoRGB(0.3, 1.0, 0.5, 1.0)
+colors.dark_green = reaper.ImGui_ColorConvertHSVtoRGB(0.3, 1.0, 0.4, 1.0)
+colors.mid_green = reaper.ImGui_ColorConvertHSVtoRGB(0.3, 1.0, 0.45, 1.0)
+
+
+colors.grey = reaper.ImGui_ColorConvertHSVtoRGB(0.0, 0.0, 0.63, 1.0)
+colors.dark_grey = reaper.ImGui_ColorConvertHSVtoRGB(0.0, 0.0, 0.47, 1.0)
+colors.mid_grey = reaper.ImGui_ColorConvertHSVtoRGB(0.0, 0.0, 0.55, 1.0)
+
+return colors
