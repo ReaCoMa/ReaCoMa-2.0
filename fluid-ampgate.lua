7ca3007e37e823de27c1ef218c2fb38a37eee8b7
James Bradbury <jamesbradbury93@gmail.com>
2021-07-01 19:08:28 +0100

    rough implementation of ampgate

---
+++ fluid-ampgate.lua
@@ -0,0 +1,16 @@
+local info = debug.getinfo(1,'S');
+local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+loadfile(script_path .. "lib/reacoma.lua")()
+if reacoma.settings.fatal then return end
+
+obj = reacoma.ampgate
+reacoma.params.get(obj)
+
+ctx = imgui_helpers.create_context(obj.info.algorithm_name, 454, 287)
+viewport = reaper.ImGui_GetMainViewport(ctx)
+
+reaper.defer(
+    function()
+        reacoma.imgui_wrapper.loop(ctx, viewport, state, obj, reacoma.settings.slice_preview)
+    end
+)
