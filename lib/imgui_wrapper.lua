9c71f4f2b31cee9575e1c67cc142bd44174bcbd3
James Bradbury <jamesbradbury93@gmail.com>
2021-07-01 17:36:15 +0100

    basic prototype for path setting

--- lib/imgui_wrapper.lua
+++ lib/imgui_wrapper.lua
@@ -1,5 +1,10 @@
 imgui_wrapper = {}
 
+local r = reaper
+local path_valid = false
+local show_modal = true
+local confirmed_path_valid = false
+
 -- FRAME LOOP --
 imgui_wrapper.loop = function(ctx, viewport, state, obj, preview)
     if reaper.ImGui_IsCloseRequested(ctx) then
@@ -8,30 +13,115 @@ imgui_wrapper.loop = function(ctx, viewport, state, obj, preview)
         reacoma.params.set(obj)
         return
     end
-    
+
     reaper.ImGui_SetNextWindowPos(ctx, reaper.ImGui_Viewport_GetPos(viewport))
     reaper.ImGui_SetNextWindowSize(ctx, reaper.ImGui_Viewport_GetSize(viewport))
-    reaper.ImGui_Begin(ctx, 'wnd', nil, reaper.ImGui_WindowFlags_NoDecoration())
 
-    w, h = reaper.ImGui_Viewport_GetSize(viewport)
-    ---------------------------------  FRAME  -------------------------
-    -- If you touch the process button, or press enter
-    if reaper.ImGui_Button(ctx, obj.info.action) or reaper.ImGui_IsKeyPressed(ctx, 13) then
-        state = reacoma.imgui_helpers.process(obj) -- TODO: make this respond to slicer/layers
+    if confirmed_path_valid == false then
+        -- PATH CHECKING --
+    
+        if show_modal then
+            r.ImGui_OpenPopup(ctx, 'Set ReaCoMa Path')
+
+            -- Always center this window when appearing
+            local center = {r.ImGui_Viewport_GetCenter(r.ImGui_GetMainViewport(ctx))}
+            local window_size = r.ImGui_Viewport_GetSize(viewport)
+            -- r.ImGui_SetNextWindowSize(ctx, 400, 230, 0)
+            -- r.ImGui_SetNextWindowPos(ctx, center[1], center[2], r.ImGui_Cond_Appearing(), 0.5, 0.5)
+
+            -- ENTER MODAL --
+            r.ImGui_BeginPopupModal(ctx, 'Set ReaCoMa Path', nil, r.ImGui_WindowFlags_AlwaysAutoResize())
+            r.ImGui_TextWrapped(ctx, 'The path to the FluCoMa CLI tools is not set. Please follow the next prompt to configure it. Doing so remains persistent across projects and sessions of reaper.')
+            r.ImGui_TextWrapped(ctx, "For example, if you've just downloaded the tools from the flucoma.org/download then you'll need to provide the path to the 'bin' folder which is inside 'Fluid Corpus Manipulation'.")
+            
+            if r.ImGui_Button(ctx, 'OK', 120, 0) then 
+                show_modal = false
+                r.ImGui_CloseCurrentPopup(ctx) 
+            end
+            r.ImGui_EndPopup(ctx)
+            -- EXIT MODAL --
+        end
+
+        reaper.ImGui_Begin(ctx, 'wnd', nil, reaper.ImGui_WindowFlags_NoDecoration())
+
+        r.ImGui_Text(ctx, 'FluCoMa Command Line Tools Path')
+
+
+        if path_valid then 
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_FrameBg(), colors.green)
+        else
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_FrameBg(), colors.red)
+        end
+
+        _, reacoma.settings.path = r.ImGui_InputText(ctx, '', reacoma.settings.path)
+        r.ImGui_PopStyleColor(ctx)
+        if path_valid then
+            r.ImGui_SameLine(ctx)
+            r.ImGui_Text(ctx, 'Path looks good!')
+        end
+
+        r.ImGui_BeginChildFrame(ctx, '##file-drop', 0, 100)
+        r.ImGui_Text(ctx, 'Drag and drop the bin folder here...')
+        r.ImGui_EndChildFrame(ctx)
+
+        if r.ImGui_BeginDragDropTarget(ctx) then
+            local rv, count = r.ImGui_AcceptDragDropPayloadFiles(ctx)
+            if rv then
+                _, reacoma.settings.path = r.ImGui_GetDragDropPayloadFile(ctx, 0)
+            end
+            r.ImGui_EndDragDropTarget(ctx)
+        end
+
+        -- CONFIRM BUTTON --
+        if path_valid then 
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_Button(), colors.dark_green)
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_ButtonActive(), colors.mid_green)
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_ButtonHovered(), colors.green)
+        else
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_Button(), colors.dark_grey)
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_ButtonActive(), colors.mid_grey)
+            r.ImGui_PushStyleColor(ctx, r.ImGui_Col_ButtonHovered(), colors.grey)
+        end
+        
+        if reaper.ImGui_Button(ctx, 'confirm path') then
+            if path_valid then
+                confirmed_path_valid = path_valid
+            else
+                -- modal popup
+            end
+        end
+        r.ImGui_PopStyleColor(ctx, 3)
+
+        path_valid = reacoma.paths.is_path_valid(reacoma.settings.path)
+        
+        if path_valid then
+            r.ImGui_SameLine(ctx)
+            r.ImGui_Text(ctx, '<-- Now hit this button to store the path!')
+        end
+        reaper.ImGui_End(ctx)
     end
-    reaper.ImGui_SameLine(ctx)
-    reaper.ImGui_Text(ctx, w..' x '..h)
+    
+    -- GUI --
+    if confirmed_path_valid == true then
 
-    if obj.info.action == 'segment' then
+        reaper.ImGui_Begin(ctx, 'wnd', nil, reaper.ImGui_WindowFlags_NoDecoration())
+
+        w, h = reaper.ImGui_Viewport_GetSize(viewport)
+        if reaper.ImGui_Button(ctx, obj.info.action) or reaper.ImGui_IsKeyPressed(ctx, 13) then
+            state = reacoma.imgui_helpers.process(obj) -- TODO: make this respond to slicer/layers
+        end
         reaper.ImGui_SameLine(ctx)
-        _, preview = reaper.ImGui_Checkbox(ctx, 'preview', preview)
-    else
-        preview = 0
-    end
-    state = reacoma.imgui_helpers.update_state(ctx, obj, preview)
-    -------------------------------------------------------------------
+        reaper.ImGui_Text(ctx, w..' x '..h)
 
-    reaper.ImGui_End(ctx)
+        if obj.info.action == 'segment' then
+            reaper.ImGui_SameLine(ctx)
+            _, preview = reaper.ImGui_Checkbox(ctx, 'preview', preview)
+        else
+            preview = 0
+        end
+        state = reacoma.imgui_helpers.update_state(ctx, obj, preview)
+        reaper.ImGui_End(ctx)
+    end
     reaper.defer(
         function() 
             imgui_wrapper.loop(ctx, viewport, state, obj, preview) 
