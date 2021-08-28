86762c2941442e4a40296940da46da4dd4da52c6
James Bradbury <jamesbradbury93@gmail.com>
2021-08-28 12:22:10 +0100

    update script for unix

--- install_reacoma.sh
+++ install_reacoma.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
-BOLD=$(tput bold)
-NORMAL=$(tput sgr0)
-RED='\033[0;31m'
-BRORANGE='\033[0:33m'
-YELLOW='\033[1:33m'
 
-NC='\033[0m' # No Color
-
-echo "$BOLD---- ReaCoMa 2.0 Installer Script ----$NORMAL"
+echo "---- ReaCoMa 2.0 Installer Script ----"
 echo "This installer will download ReaCoMa, ReaImGui and the FluCoMa CLI Executables.\nAll temporary files such as .zip will be downloaded to your user downloads folder and removed afterwards\n"
 
 ARCH=`uname -m`
@@ -30,22 +23,22 @@ fi
 if [ -d "$REACOMA_LOCATION" ]
 then
     cd $REACOMA_LOCATION && git pull > /dev/null 2>&1
-    echo "$BRORANGE\n1. ReaCoMa exists at $REACOMA_LOCATION so invoked git pull"
+    echo "1. ReaCoMa exists at $REACOMA_LOCATION so invoked git pull"
 else
     git clone https://github.com/ReaCoMa/ReaCoMa-2.0.git "$REACOMA_LOCATION" > /dev/null 2>&1
-    echo "$BRORANGE\n1. ReaCoMa downloaded to $REACOMA_LOCATION"
+    echo "1. ReaCoMa downloaded to $REACOMA_LOCATION"
 fi
 
 
 if [ $ARCH == "arm64" ]
 then
-    echo "$BRORANGE\n2. ARM64 Architecture Identified for ImGui$NC"
+    echo "2. ARM64 Architecture Identified for ImGui"
     FILE="reaper_imgui-arm64$EXT"
 fi
 
 if [ $ARCH == "x86_64" ]
 then
-    echo "$BRORANGE\n2. x86_64 architecture identified for ImGui$NC"
+    echo "2. x86_64 architecture identified for ImGui"
     FILE="reaper_imgui-x86_64$EXT"
 fi
 
@@ -60,15 +53,18 @@ else
     FLUCOMA_RELEASE="https://github.com/flucoma/flucoma-cli/releases/download/1.0.0.RC1b/FluCoMa-CLI-Linux-RC1b.zip"
 fi
 
+# make the folder for the binaries
+BINARY_LOCATION="$REACOMA_LOCATION/bin"
+mkdir -p $BINARY_LOCATION
+
 ZIP_LOCATION="$HOME/Downloads/flucoma-cli.zip"
 curl -s -L $FLUCOMA_RELEASE --output $ZIP_LOCATION
 unzip -o -q "$HOME/Downloads/flucoma-cli.zip" -d "$HOME/Downloads/"
-mkdir -p "$HOME/.bin/flucoma-cli"
-cp -r "$HOME/Downloads/FluidCorpusManipulation/bin/" "$HOME/.bin/flucoma-cli/"
-echo "$BRORANGE\n3. Executables copied to $HOME/.bin/flucoma-cli$NC"
+cp -r "$HOME/Downloads/FluidCorpusManipulation/bin/" $BINARY_LOCATION
+echo "3. Executables copied to $BINARY_LOCATION"
 
-echo "$BRORANGE\n4. Cleaning Up Files...$NC"
+echo "4. Cleaning Up Files..."
 rm -r $ZIP_LOCATION
 rm -r "$HOME/Downloads/FluidCorpusManipulation"
 
-echo "$RED\nDONE! Now run one of the scripts from REAPER"
+echo "DONE! Now run one of the scripts from REAPER"
