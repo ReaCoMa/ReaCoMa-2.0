61087e0498153a50761af833d55869483f4e10fe
James Bradbury <jamesbradbury93@gmail.com>
2021-08-29 10:36:18 +0100

    make script a bit more robust

--- install_reacoma.sh
+++ install_reacoma.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "---- ReaCoMa 2.0 Installer Script ----"
 echo "This installer will download ReaCoMa, ReaImGui and the FluCoMa CLI Executables.\nAll temporary files such as .zip will be downloaded to your user downloads folder and removed afterwards\n"
@@ -10,8 +10,8 @@ REAIMGUI_VERSIONED_URL="https://github.com/cfillion/reaimgui/releases/download/v
 # The extension of the shared library will be determined by the DISTRO
 if [ $DISTRO == "Darwin" ]
 then
-    REACOMA_LOCATION="$HOME/Library/ApplicationSupport/REAPER/Scripts/ReaCoMa-2.0"
-    DYLIB_OUTPUT="$HOME/Library/ApplicationSupport/REAPER/UserPlugins" 
+    REACOMA_LOCATION="$HOME/Library/Application Support/REAPER/Scripts/ReaCoMa-2.0"
+    DYLIB_OUTPUT="$HOME/Library/Application Support/REAPER/UserPlugins" 
     EXT=".dylib"
 else
     REACOMA_LOCATION="$HOME/.config/REAPER/Scripts/ReaCoMa-2.0"
@@ -22,21 +22,21 @@ fi
 # Download ReaCoMa to the UserScripts place
 if [ -d "$REACOMA_LOCATION" ]
 then
-    cd $REACOMA_LOCATION && git pull > /dev/null 2>&1
-    echo "1. ReaCoMa exists at $REACOMA_LOCATION so invoked git pull"
+    cd "$REACOMA_LOCATION" && git pull > /dev/null 2>&1
+    echo "1. ReaCoMa exists at '$REACOMA_LOCATION' so invoked git pull"
 else
     git clone https://github.com/ReaCoMa/ReaCoMa-2.0.git "$REACOMA_LOCATION" > /dev/null 2>&1
-    echo "1. ReaCoMa downloaded to $REACOMA_LOCATION"
+    echo "1. ReaCoMa downloaded to '$REACOMA_LOCATION'"
 fi
 
 
-if [ $ARCH == "arm64" ]
+if [ "$ARCH" == "arm64" ]
 then
     echo "2. ARM64 Architecture Identified for ImGui"
     FILE="reaper_imgui-arm64$EXT"
 fi
 
-if [ $ARCH == "x86_64" ]
+if [ "$ARCH" == "x86_64" ]
 then
     echo "2. x86_64 architecture identified for ImGui"
     FILE="reaper_imgui-x86_64$EXT"
@@ -46,7 +46,7 @@ CONCAT_OUTPUT="$DYLIB_OUTPUT/$FILE"
 curl -s -L "$REAIMGUI_VERSIONED_URL/$FILE" --output "$DYLIB_OUTPUT/$FILE" >> /dev/null
 
 # Get FluCoMa CLI Tools
-if [ $DISTRO == "Darwin" ]
+if [ "$DISTRO" == "Darwin" ]
 then
     FLUCOMA_RELEASE="https://github.com/flucoma/flucoma-cli/releases/download/1.0.0.RC1b/FluCoMa-CLI-Mac-RC1b.zip"
 else
@@ -55,16 +55,16 @@ fi
 
 # make the folder for the binaries
 BINARY_LOCATION="$REACOMA_LOCATION/bin"
-mkdir -p $BINARY_LOCATION
+mkdir -p "$BINARY_LOCATION"
 
 ZIP_LOCATION="$HOME/Downloads/flucoma-cli.zip"
-curl -s -L $FLUCOMA_RELEASE --output $ZIP_LOCATION
+curl -s -L "$FLUCOMA_RELEASE" --output "$ZIP_LOCATION"
 unzip -o -q "$HOME/Downloads/flucoma-cli.zip" -d "$HOME/Downloads/"
-cp -a "$HOME/Downloads/FluidCorpusManipulation/bin/." $BINARY_LOCATION
+cp -a "$HOME/Downloads/FluidCorpusManipulation/bin/." "$BINARY_LOCATION"
 echo "3. Executables copied to $BINARY_LOCATION"
 
 echo "4. Cleaning Up Files..."
-rm -r $ZIP_LOCATION
+rm -r "$ZIP_LOCATION"
 rm -r "$HOME/Downloads/FluidCorpusManipulation"
 
 echo "DONE! Now run one of the scripts from REAPER"
