3f36d290129d6d6fe09e1196c2fb70bc7ab2901e
James Bradbury <me@jamesbradbury.net>
2021-10-31 00:00:12 +0100

    update metapackage

--- scripts/ReaCoMa.lua
+++ scripts/ReaCoMa.lua
@@ -6,12 +6,13 @@ Provides:
     [main] fluid-ampgate.lua 
     [main] fluid-ampslice.lua
     [main] fluid-hpss.lua
-    [main] fluid.nmf.lua
+    [main] fluid-nmf.lua
     [main] fluid-noveltyslice.lua
     [main] fluid-onsetslice.lua
     [main] fluid-sines.lua
     [main] fluid-transients.lua
     [main] fluid-transientslice.lua
     [nomain] lib/*
+    [nomain] clean_path.lua
 Metapackage: true
 ]]
