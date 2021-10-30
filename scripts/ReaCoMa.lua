0bb003e1ecbc614837520bcd4330413f6d2c6971
James Bradbury <me@jamesbradbury.net>
2021-10-31 00:58:10 +0100

    add about me

--- scripts/ReaCoMa.lua
+++ scripts/ReaCoMa.lua
@@ -22,4 +22,22 @@ Links:
     Tutorials https://www.youtube.com/watch?v=r3uHMXmlPRo&list=PLCQRw62RgghbsZgsA98lLkOwjBSs4yc9T
     Creator https://jamesbradbury.net
     FluCoMa https://flucoma.org
+About:
+    # ReaCoMa
+
+    ReaCoMa brings the Fluid Corpus Manipulation (FluComa) toolkit to REAPER as a set of flexible ReaScripts.
+
+    This includes algorithms for:
+    * harmonic-percussive source separation
+    * non-negative matrix factorisation (blind source separation)
+    * sinusoidal modelling and decomposition
+    * transient extraction
+    
+    as well as several flavours of audio segmentation:
+    * novelty slice
+    * onset slice
+    * relative and absolute amplitude slicing
+    * transient slicing
+
+    ReaCoMa depends on [ReaImGui](https://github.com/cfillion/reaimgui) which can be managed manually or via ReaPack.
 ]]
