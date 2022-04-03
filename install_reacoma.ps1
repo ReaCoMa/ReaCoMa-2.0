0be26cb38a3702015ae635620b981d899f166ba0
James Bradbury <github@jamesbradbury.net>
2022-04-04 05:43:41 +0800

    update reaimgui dep

--- install_reacoma.ps1
+++ install_reacoma.ps1
@@ -3,7 +3,7 @@ write-output "Please CLOSE REAPER for this script to work."
 $resource_path = ($env:USERPROFILE+'\AppData\Roaming\REAPER')
 $reacoma_dist = "https://github.com/ReaCoMa/ReaCoMa-2.0/archive/refs/heads/main.zip"
 $reacoma_location = ($resource_path+'\Scripts\')
-$reaimgui_dist = "https://github.com/cfillion/reaimgui/releases/download/v0.5.4/reaper_imgui-x64.dll"
+$reaimgui_dist = "https://github.com/cfillion/reaimgui/releases/download/v0.6.1/reaper_imgui-x64.dll"
 $flucoma_dist = "https://github.com/flucoma/flucoma-cli/releases/download/1.0.0-TB2.beta6/FluCoMa-CLI-Windows.zip"
 $flucoma_dl = ($env:USERPROFILE+'\Downloads\flucoma-binaries.zip')
 $flucoma_location = ($resource_path + '\Scripts\ReaCoMa-2.0-main\bin')
