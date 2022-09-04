75ceb8871b3d5c181b99ffeb5cd73fa7d9f07c9f
James Bradbury <github@jamesbradbury.net>
2022-09-05 00:20:28 +0100

    updated scripts for installation

--- create_release_win.sh
+++ create_release_win.sh
@@ -1,26 +1,22 @@
 NAME="ReaCoMa 2.0"
-CLI="https://github.com/flucoma/flucoma-cli/releases/download/1.0.4/FluCoMa-CLI-Mac.dmg"
-IMGUI="https://github.com/cfillion/reaimgui/releases/download/v0.7"
+CLI="https://github.com/flucoma/flucoma-cli/releases/download/1.0.4/FluCoMa-CLI-Win.zip"
 
 # Get CLI Binaries
 curl -s -L "$CLI" --output cli.dmg
 hdiutil attach cli.dmg
-cp -a /Volumes/FluCoMa-CLI-Mac/FluidCorpusManipulation/bin .
+cp -a /Volumes/FluCoMa-CLI-Win/FluidCorpusManipulation/bin .
 hdiutil detach /Volumes/FluCoMa-CLI-Mac
 rm cli.dmg
 
-
-# Create DMG
+# Create ZIP
 test -f "$NAME.dmg" && rm -f "$NAME.dmg"
 test -f release && rm -rf release
 mkdir -p release
 mkdir -p "release/$NAME"
 rsync -av -q . "release/$NAME" --exclude "$NAME" --exclude "REAPER Scripts" --exclude create_release_win.sh --exclude create_release_win.sh --exclude .github --exclude release --exclude assets --exclude tests --exclude install.bat
 
-appdmg ./dmg.json ./ReaCoMa\ 2.0.dmg
+rsync -av -q install.bat "release"
 
-# Alias(es)
-# test -f "REAPER Scripts" && rm -f "REAPER Scripts"
-# ln -s ~/Library/Application\ Support/REAPER/Scripts REAPER\ Scripts
+zip -r "ReaCoMa 2.0.zip" release
 
 rm -rf release
