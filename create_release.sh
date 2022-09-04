cfe0a1df60d753cbff33f9dc2b63db24df5ab863
James Bradbury <github@jamesbradbury.net>
2022-09-04 22:48:05 +0100

    update create_release

--- create_release.sh
+++ create_release.sh
@@ -9,17 +9,18 @@ cp -a /Volumes/FluCoMa-CLI-Mac/FluidCorpusManipulation/bin .
 hdiutil detach /Volumes/FluCoMa-CLI-Mac
 rm cli.dmg
 
-# Alias(es)
-test -f "REAPER Scripts" && rm -f "REAPER Scripts"
-ln -s ~/Library/Application\ Support/REAPER/Scripts REAPER\ Scripts
 
 # Create DMG
 test -f "$NAME.dmg" && rm -f "$NAME.dmg"
 test -f release && rm -rf release
 mkdir -p release
 mkdir -p "release/$NAME"
-rsync -av -q . "release/$NAME" --exclude "$NAME" --exclude "REAPER Scripts" --exclude create_release.sh --exclude .github --exclude release --exclude assets
+rsync -av -q . "release/$NAME" --exclude "$NAME" --exclude "REAPER Scripts" --exclude create_release.sh --exclude .github --exclude release --exclude assets --exclude tests
 
-create-dmg --add-file "REAPER Scripts" "REAPER Scripts" 300 150 --add-file "Quick Start.rtf" "Quick Start.rtf" 30 30 --volname "$NAME" --volicon "assets/icon.icns" --window-size 500 300 --window-pos 0 0 --icon "ReaCoMa 2.0" 30 150 --icon-size 48 "$NAME.dmg" release
+appdmg ./dmg.json ./ReaCoMa\ 2.0.dmg
+
+# Alias(es)
+# test -f "REAPER Scripts" && rm -f "REAPER Scripts"
+# ln -s ~/Library/Application\ Support/REAPER/Scripts REAPER\ Scripts
 
 rm -rf release
