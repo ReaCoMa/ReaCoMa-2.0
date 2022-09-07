ca07bf4f71f5bca82f69cb3f5f08f359c313b4bd
James Bradbury <github@jamesbradbury.net>
2022-09-07 11:45:48 +0100

    update release

--- distribution/create_release.sh
+++ distribution/create_release.sh
@@ -1,14 +1,28 @@
 NAME="ReaCoMa 2.0"
-CLI="https://github.com/flucoma/flucoma-cli/releases/download/1.0.4/FluCoMa-CLI-Mac.dmg"
+CLI_VERSION="1.0.4"
 OS="$1"
 
 test -f release && rm -rf release
+rm -rf bin
 
 if [ "$OS" == "win" ]; then
+	rm -rf FluidCorpusManipulation
+	CLI="https://github.com/flucoma/flucoma-cli/releases/download/$CLI_VERSION/FluCoMa-CLI-Windows.zip"
+	curl -s -L "$CLI" --output cli.zip
+	unzip -q cli.zip
+	mv FluidCorpusManipulation/bin .
+	rm -rf FluidCorpusManipulation
+	rm cli.zip
 
-fi
+	# # Create a ZIP
+	mkdir -p release
+	mkdir -p "release/$NAME"
+	rsync -av -q --exclude=.github --exclude=.git --exclude=assets --exclude=tests --exclude=distribution . "release/$NAME"
+	cp distribution/install.bat release
 
-if [ "$OS" == "mac"]; then
+	zip -r ReaCoMa\ 2.0.zip release
+else
+	CLI="https://github.com/flucoma/flucoma-cli/releases/download/$CLI_VERSION/FluCoMa-CLI-Mac.dmg"
 	# Get CLI Binaries
 	curl -s -L "$CLI" --output cli.dmg
 	hdiutil attach cli.dmg
