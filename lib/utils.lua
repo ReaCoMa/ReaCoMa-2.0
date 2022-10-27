7028a57a3f67282b1a67c69458d9eeb870e3433d
James Bradbury <github@jamesbradbury.net>
2022-10-27 16:55:55 +0200

    blank

--- lib/utils.lua
+++ lib/utils.lua
@@ -4,7 +4,6 @@ utils.bool_to_number = { [true]=1, [false]=0 }
 utils.string_to_bool = { ['true']=true, ['false']=false }
 utils.bool_to_string = { [true]='true', [false]='false' }
 
-
 utils.DEBUG = function(string)
     -- Handy function for quickly debugging strings
     reaper.ShowConsoleMsg(tostring(string))
