6592dc01a7535846042a16ef3b2a674442313ba2
James Bradbury <github@jamesbradbury.net>
2022-10-28 15:30:01 +0200

    also update playrate and reverse flag

--- lib/layers.lua
+++ lib/layers.lua
@@ -29,20 +29,24 @@ layers.process = function(item_index, data)
         reaper.InsertMedia(data.outputs[k][item_index], 3)
         local item = reaper.GetSelectedMediaItem(0, 0)
         local take = reaper.GetActiveTake(item)
-        local src = reaper.GetMediaItemTake_Source(take)
         reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", data.playrate[item_index])
         reaper.SetMediaItemTakeInfo_Value(take, "I_PITCHMODE", data.playtype[item_index])
         if data.reverse[item_index] then reaper.Main_OnCommand(41051, 0) end
     end
 end
 
-layers.process_matrix = function(source, target, output)
-    reaper.SetMediaItemSelected(target.item, true)
+layers.process_matrix = function(a, b, output, append_target)
+    if append_target == 0 then
+        reaper.SetMediaItemSelected(a.item, true)
+    else
+        reaper.SetMediaItemSelected(b.item, true)
+    end
     reaper.InsertMedia(output, 3)
-    -- reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", data.playrate[item_index])
-    -- reaper.SetMediaItemTakeInfo_Value(take, "I_PITCHMODE", data.playtype[item_index])
-    -- if data.reverse[item_index] then reaper.Main_OnCommand(41051, 0) end
-
+    local item = reaper.GetSelectedMediaItem(0, 0) -- get the newly added take
+    local take = reaper.GetActiveTake(item)
+    reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", b.playrate)
+    reaper.SetMediaItemTakeInfo_Value(take, "I_PITCHMODE", b.playtype)
+    if b.reverse then reaper.Main_OnCommand(41051, 0) end
 end
 
 return layers
