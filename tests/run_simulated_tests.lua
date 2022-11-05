cd634334b4b238fec0c24ae13c179cc1a6bcc4cf
James Bradbury <github@jamesbradbury.net>
2022-11-05 23:50:13 +0100

    run onsetslice test in runer

--- tests/run_simulated_tests.lua
+++ tests/run_simulated_tests.lua
@@ -1,4 +1,5 @@
 local info = debug.getinfo(1,'S');
 local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 
-dofile(script_path..'test_noveltyslice/test.lua')
+dofile(script_path .. 'test_noveltyslice/test.lua')
+dofile(script_path .. 'test_onsetslice/test.lua')
