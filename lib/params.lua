48ca1eba3fcf34f29d38591c23b9165498f7d56c
James Bradbury <github@jamesbradbury.net>
2023-07-02 17:30:02 +0200

    namespace parameter restoration to version of reacoma to avoid conflicts

--- lib/params.lua
+++ lib/params.lua
@@ -3,29 +3,35 @@ params = {}
 -- So we don't have to figure out what the index of a table is
 -- for any given default parameters. We can encapsulate it into
 -- a function that just does it for us.
-params.enum_default_index = function(obj)
-
+-- TODO: one day genercise all the functions into a table utilities module...
+params.find_index = function(tbl, value)
+	for i, v in ipairs(tbl) do
+	  if v == value then
+		return i
+	  end
+	end
+	return nil
 end
 
 params.set = function(obj)
     for _, param in pairs(obj.parameters) do
         -- Handle custom parameters
         if reacoma.utils.table_has(reacoma.widgets, param.widget) then
-            reaper.SetExtState(obj.info.ext_name, param.name, param.index, true)
+            reaper.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.index, true)
         else
-            reaper.SetExtState(obj.info.ext_name, param.name, param.value, true)
+            reaper.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.value, true)
         end
     end
 end
 
 params.get = function(obj)
     for _, param in pairs(obj.parameters) do
-        if reaper.HasExtState(obj.info.ext_name, param.name) then
+        if reaper.HasExtState(reacoma.settings.version..obj.info.ext_name, param.name) then
             -- Test if parameter is a custom widget
             if reacoma.utils.table_has(reacoma.widgets, param.widget) then
-                param.index = reaper.GetExtState(obj.info.ext_name, param.name)
+                param.index = reaper.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
             else
-                param.value = reaper.GetExtState(obj.info.ext_name, param.name)
+                param.value = reaper.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
             end
         end
     end
