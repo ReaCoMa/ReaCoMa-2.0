6fad8908fa25e27ef6d8d377f255233ac0139aef
James Bradbury <github@jamesbradbury.net>
2023-07-02 21:42:14 +0200

    cleanup params code

--- lib/params.lua
+++ lib/params.lua
@@ -1,4 +1,5 @@
-params = {}
+local r = reaper
+local params = {}
 
 -- So we don't have to figure out what the index of a table is
 -- for any given default parameters. We can encapsulate it into
@@ -17,21 +18,21 @@ params.set = function(obj)
     for _, param in pairs(obj.parameters) do
         -- Handle custom parameters
         if reacoma.utils.table_has(reacoma.imgui.widgets, param.widget) then
-            reaper.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.index, true)
+            r.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.index, true)
         else
-            reaper.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.value, true)
+            r.SetExtState(reacoma.settings.version..obj.info.ext_name, param.name, param.value, true)
         end
     end
 end
 
 params.get = function(obj)
     for _, param in pairs(obj.parameters) do
-        if reaper.HasExtState(reacoma.settings.version..obj.info.ext_name, param.name) then
+        if r.HasExtState(reacoma.settings.version..obj.info.ext_name, param.name) then
             -- Test if parameter is a custom widget
             if reacoma.utils.table_has(reacoma.imgui.widgets, param.widget) then
-                param.index = reaper.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
+                param.index = r.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
             else
-                param.value = reaper.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
+                param.value = r.GetExtState(reacoma.settings.version..obj.info.ext_name, param.name)
             end
         end
     end
@@ -40,8 +41,8 @@ end
 -- TODO store the default parameters
 -- This function is currently not being used for anything
 params.store_defaults = function(obj)
-    idx = 1
-    defaults = {}
+    local idx = 1
+    local defaults = {}
     for _, param in pairs(obj.parameters) do
         defaults[idx] = param.value
         idx = idx + 1
@@ -50,7 +51,7 @@ params.store_defaults = function(obj)
 end
 
 params.restore_defaults = function(obj)
-    idx = 1
+    local idx = 1
     for _, param in pairs(obj.parameters) do
         param.value = obj.defaults[idx]
         idx = idx + 1
