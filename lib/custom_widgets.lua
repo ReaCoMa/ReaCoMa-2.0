2a5e6bbafa3740889ccb6c4460f5cc6dbd16d76c
James Bradbury <github@jamesbradbury.net>
2023-07-02 11:48:43 +0200

    add custom widgets module

---
+++ lib/custom_widgets.lua
@@ -0,0 +1,47 @@
+local widgets = {}
+
+function generate_odds(start, finish)
+	local odd_tbl = {}
+	
+	-- Ensure the startNumber is odd
+	if start % 2 == 0 then
+	  start = start + 1
+	end
+	
+	for i = start, finish, 2 do
+	  table.insert(odd_tbl, i)
+	end
+	
+	return odd_tbl
+end
+
+function generate_powers_of_two(start_number, end_number)
+	local powers_table = {}
+	
+	for power = 0, end_number do
+	  local value = 2 ^ power
+	  if value >= start_number and value <= end_number then
+		table.insert(powers_table, value)
+	  end
+	end
+	
+	return powers_table
+end
+
+-- For FFT parameters
+widgets.FFTSlider = {
+	opts = generate_powers_of_two(64, 32768)
+}
+
+-- For parameters which snap to odds like filters or kernel sizes
+widgets.FilterSlider = {
+	opts = generate_odds(1, 101)
+}
+
+widgets.KernelSlider = {
+	opts = generate_odds(3, 71)
+}
+
+
+
+return widgets
