f120cacae9f3cb17d04d73f9fd67f86083f0251b
James Bradbury <github@jamesbradbury.net>
2023-07-03 12:52:07 +0200

    namespace by version

--- utility_scripts/clean_parameter_data.lua
+++ utility_scripts/clean_parameter_data.lua
@@ -3,10 +3,15 @@ local script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
 loadfile(script_path .. "../lib/reacoma.lua")()
 if reacoma.settings.fatal then return end
 
-for k, v in pairs(reacoma.algorithms) do
+for _, v in pairs(reacoma.algorithms) do
 	for i=1, #v.parameters do
 		param = v.parameters[i]
-		reaper.DeleteExtState(v.info.ext_name, param.name, true)
+		if reaper.HasExtState(reacoma.settings.version..v.info.ext_name, param.name) then
+			reaper.DeleteExtState(reacoma.settings.version..v.info.ext_name, param.name, true)
+			reaper.ShowConsoleMsg('Deleted '..param.name..'\n')
+		else
+			reaper.ShowConsoleMsg(param.name..' did not have anything stored\n')
+		end
 	end
 end
 
