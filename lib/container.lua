67605187d94db6a4b7deb61e8addfde46d9b3a2b
James Bradbury <github@jamesbradbury.net>
2023-07-03 12:52:30 +0200

    further refactoring

--- lib/container.lua
+++ lib/container.lua
@@ -1,4 +1,6 @@
-container = {}
+local r = reaper
+
+local container = {}
 
 container.generic = {
     full_path = {},
@@ -30,40 +32,39 @@ container.get_data = function(i, data)
 end
 
 container.get_item_info = function(item_index)
-    local info = {}
-    local item = reaper.GetSelectedMediaItem(0, item_index-1)
+    local item = r.GetSelectedMediaItem(0, item_index-1)
     return container.get_take_info_from_item(item)
     
 end
 
 container.get_take_info_from_item = function(item)
-    local take = reaper.GetActiveTake(item)
-    local take_markers = reaper.GetNumTakeMarkers(take)
-    local src = reaper.GetMediaItemTake_Source(take)
-    local src_parent = reaper.GetMediaSourceParent(src)
+    local take = r.GetActiveTake(item)
+    local take_markers = r.GetNumTakeMarkers(take)
+    local src = r.GetMediaItemTake_Source(take)
+    local src_parent = r.GetMediaSourceParent(src)
     local sr = nil
     local full_path = nil
     local reverse = nil
     
     if src_parent ~= nil then
-        sr = reaper.GetMediaSourceSampleRate(src_parent)
-        full_path = reaper.GetMediaSourceFileName(src_parent, "")
+        sr = r.GetMediaSourceSampleRate(src_parent)
+        full_path = r.GetMediaSourceFileName(src_parent, "")
         reverse = true
     else
-        sr = reaper.GetMediaSourceSampleRate(src)
-        full_path = reaper.GetMediaSourceFileName(src, "")
+        sr = r.GetMediaSourceSampleRate(src)
+        full_path = r.GetMediaSourceFileName(src, "")
         reverse = false
     end
     
     -- Now check the full path works
     reacoma.utils.check_extension(full_path)
 
-    local playrate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
-    local take_ofs = reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
-    local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    local src_len = reaper.GetMediaSourceLength(src)
-    local item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH") * playrate
-    local playtype  = reaper.GetMediaItemTakeInfo_Value(take, "I_PITCHMODE")
+    local playrate = r.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
+    local take_ofs = r.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS")
+    local item_pos = r.GetMediaItemInfo_Value(item, "D_POSITION")
+    local src_len = r.GetMediaSourceLength(src)
+    local item_len = r.GetMediaItemInfo_Value(item, "D_LENGTH") * playrate
+    local playtype  = r.GetMediaItemTakeInfo_Value(take, "I_PITCHMODE")
 
     if reverse then
         take_ofs = abs(src_len - (item_len + take_ofs))
@@ -73,9 +74,9 @@ container.get_take_info_from_item = function(item)
         item_len = ((src_len-take_ofs) * (1 / playrate))
     end
 
-    local take_ofs_samples = utils.stosamps(take_ofs, sr)
-    local item_pos_samples = utils.stosamps(item_pos, sr)
-    local item_len_samples = floor(utils.stosamps(item_len, sr))
+    local take_ofs_samples = reacoma.utils.stosamps(take_ofs, sr)
+    local item_pos_samples = reacoma.utils.stosamps(item_pos, sr)
+    local item_len_samples = floor(reacoma.utils.stosamps(item_len, sr))
 
     -- Yeah this is verbose but it makes it cleaner
     local info = {
@@ -98,7 +99,8 @@ container.get_take_info_from_item = function(item)
     info.playtype = playtype
     info.path = reacoma.utils.form_path(full_path)
     -- Slicing specific stuff
-    info.tmp = full_path .. utils.uuid(item_index or -1) .. "fs.csv"
+    -- TODO: this is weird and item_index needs to be dealt with
+    info.tmp = full_path .. reacoma.utils.uuid(item_index or -1) .. "fs.csv"
 
     return info
 end
