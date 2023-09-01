5399c62c95a89ffba1ff22e661571c9b8dca74c6
James Bradbury <github@jamesbradbury.net>
2023-09-02 00:03:55 +0200

    improve ini parsing to allow inline comments

--- lib/configuration.lua
+++ lib/configuration.lua
@@ -2,59 +2,38 @@ local configuration = {}
 
 local r = reaper
 
-configuration.get_ini_value = function(ini_file_name, section, key)
-	-- Check that file exists in the first place
-	if not reaper.file_exists(ini_file_name) then return false end
+function parse_ini_file(file_path)
+    local ini_data = {}
+    local current_section = nil
 
-	local section_found = false
-	local key_found = false
-	for line in io.lines(ini_file_name) do
-		-- Try to find the section
-		if not section_found and line == "[" .. section .. "]" then
-			section_found = true
-		end
-		
-		-- Section was found, but it has no keys (-> return and show error message)
-		if section_found and starts_with(line, "[") and line ~= "[" .. section .. "]" then
-			if section_found and not key_found then 
-				-- r.ShowConsoleMsg("Couldn't find key: " .. key .. "\n") 
-			end
-			return false
-		end
-		
-		-- Section found -> try to find the key
-		if section_found then
-			if not starts_with(line, ";") then
-				local temp_line = line:match("([^=]+)")
-				if temp_line ~= nil and temp_line:trim() ~= nil then
-					temp_line = temp_line:trim()
-					if temp_line == key then
-						key_found = true
-						
-						-- Key found -> Try to get the value
-						local val = ({line:split("=")})[2]
-						-- No value set for this key -> return an empty string
-						if val == nil then
-							val = ""
-						end
-						return val:trim()
-					end
-				end
-			end
-		end
-	end
-	
-	-- Section was not found
-	if not section_found then 
-		-- r.ShowConsoleMsg("Couldn't find section: " .. section .. "\n")
-		return false
-	end
-	if not key_found then 
-		if section_found and not key_found then 
-			-- r.ShowConsoleMsg("Couldn't find key: " .. key .. "\n") 
-		end
-		return false
-	end
+    for line in io.lines(file_path) do
+        -- Remove inline comments (anything after ';' or '#')
+        line = line:gsub("[%s;#].*", "")
+
+        -- Check for section headers
+        local section = line:match("^%[([^%]]+)%]$")
+        if section then
+            current_section = section
+            ini_data[current_section] = {}
+        else
+            -- Check for key-value pairs within sections
+            local key, value = line:match("^([^=]+)=([^=]+)$")
+            if key and value and current_section then
+                key = key:match("^%s*(.-)%s*$")  -- Trim leading and trailing spaces
+                value = value:match("^%s*(.-)%s*$")
+                ini_data[current_section][key] = value
+            end
+        end
+    end
+
+    return ini_data
+end
+
+configuration.get_ini_value = function(ini_file_name, section, key)
+    -- Check that file exists in the first place
+    if not reaper.file_exists(ini_file_name) then return false end
+	local ini_data = parse_ini_file(ini_file_name)
+		return ini_data[section][key] or false
 end
 
 
